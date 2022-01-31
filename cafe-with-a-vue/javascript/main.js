9db1987227e4596d3677d1ebf245fdb779ab7bbb
Bruno Lapierre <BrunoL@MiraGeoscience.local>
2022-01-31 15:49:30 -0500

    third commit

--- cafe-with-a-vue/javascript/main.js
+++ cafe-with-a-vue/javascript/main.js
@@ -4,9 +4,28 @@ let app = new Vue({
         address: "18 avenue du Beurre, Paris, France",
         email: "hello@cafewithavue.bakery",
         phone: "01 88 88 88 88",
-        restaurantName: "La belle vue"
+        restaurantName: "La belle vue",
+        showModal: false,
+        favColor: 'blue',
+        shoppingCart: [
+            { label: 'Pommes', cost: 6 },
+            { label: 'Bananes', cost: 2 },
+            { label: 'Noix de coco', cost: 8 }
+        ],
+        apiResponse: [
+            { name: 'GitHub',  url: 'https://www.github.com' },
+            { name: 'Twitter', url: 'https://www.twitter.com' },
+            { name: 'Netlify', url: 'https://www.netlify.com' }
+        ],
     },
     methods: {
+        showColor() {
+            console.log(this.favColor);
+            alert(this.favColor);
+        },
+        changeColor(){
+            this.favColor = document.getElementById('change-color').value;
+        }
     },
     computed: {
         copyRight() {
@@ -14,5 +33,14 @@ let app = new Vue({
 
             return `Copyright - ${this.restaurantName} ${currentYear}`;
         },
+        total() {
+            let total = 0;
+
+            this.shoppingCart.forEach(item => {
+                total += item.cost
+            })
+
+            return total
+        }
     }
 })
