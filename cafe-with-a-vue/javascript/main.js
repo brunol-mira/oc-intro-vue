6c77dec016f74787c07d775970e66a6beb972ca9
Bruno Lapierre <BrunoL@MiraGeoscience.local>
2022-01-31 14:54:22 -0500

    second commit

--- cafe-with-a-vue/javascript/main.js
+++ cafe-with-a-vue/javascript/main.js
@@ -1,8 +1,18 @@
 let app = new Vue({
     el: '#app',
     data: {
-        restaurantName: 'La belle Vue',
+        address: "18 avenue du Beurre, Paris, France",
+        email: "hello@cafewithavue.bakery",
+        phone: "01 88 88 88 88",
+        restaurantName: "La belle vue"
     },
     methods: {
     },
+    computed: {
+        copyRight() {
+            const currentYear = new Date().getFullYear();
+
+            return `Copyright - ${this.restaurantName} ${currentYear}`;
+        },
+    }
 })
