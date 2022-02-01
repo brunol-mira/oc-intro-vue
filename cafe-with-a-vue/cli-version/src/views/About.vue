6387eb82c5b22508b983263dddc8cd805eed2dee
Bruno Lapierre <BrunoL@MiraGeoscience.local>
2022-02-01 17:00:28 -0500

    P2C3 Life Cycle Hooks

--- cafe-with-a-vue/cli-version/src/views/About.vue
+++ cafe-with-a-vue/cli-version/src/views/About.vue
@@ -1,5 +1,34 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Je suis un nouveau composant !</h1>
   </div>
 </template>
+
+<script>
+export default {
+  name: "Contact",
+  data() {
+    return {
+      msg: "Bonjour !",
+    }
+  },
+  beforeCreate() {
+    console.log('Je ne suis pas encore  créé')
+  },
+  created() {
+    console.log('Je suis créé !')
+  },
+  beforeMount() {
+    console.log('Je vais bientôt être monté sur le DOM!')
+  },
+  mounted() {
+    console.log('Je suis monté sur le DOM!')
+  },
+  beforeDestroy() {
+    console.log('Je suis sur le point de disparaître du DOM !')
+  },
+  destroyed() {
+    console.log('Je suis supprimé')
+  }
+}
+</script>
